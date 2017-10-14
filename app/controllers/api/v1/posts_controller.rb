d86a8b06a5261b42399a84a40235a043fd2b7442
Padraic O'Neill <pconeill@gmail.com [core]>
2017-10-14 16:45:01 -0600

    adds posts#show, confirms with postman

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -3,5 +3,10 @@ class Api::V1::PostsController < ApplicationController
   def index
     render json: Post.all
   end
-  
+
+  def show
+    id = params[:id]
+    render json: Post.where(id: id)
+  end
+
 end

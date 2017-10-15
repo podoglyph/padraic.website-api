942b0797246f9a469eef78841536bd0bcdb2985d
Padraic O'Neill <pconeill@gmail.com [core]>
2017-10-14 18:47:51 -0600

    creates user table

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
 
-  root 'api/v1/posts#index'
+  root "sessions#new"
+
   namespace :api do
     namespace :v1 do
       get '/posts', to: 'posts#index'
       get '/post/:id', to: 'posts#show'
+      resources :users, only: [:new, :create]
     end
   end
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
 end
